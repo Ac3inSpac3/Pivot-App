869cc98bb1a579c17a497ebc9f406f80c98c56c6
Ac3inSpac3 <60162987+Ac3inSpac3@users.noreply.github.com>
2024-10-01 19:49:54 +1000

    changed apple bg and date indicator

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 var APP_PREFIX = 'pivot_';
-var VERSION = 'version_079';
+var VERSION = 'version_080';
 
 // Detect the base path dynamically
 var BASE_PATH = self.location.pathname.includes('/Pivot-App/') ? '/Pivot-App/' : '/';
