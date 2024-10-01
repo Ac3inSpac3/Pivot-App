ad4765b99db225b861e946a39a7b0bba6bd694bf
Ac3inSpac3 <60162987+Ac3inSpac3@users.noreply.github.com>
2024-10-01 14:43:41 +1000

    fixed layering

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 var APP_PREFIX = 'pivot_';
-var VERSION = 'version_063';
+var VERSION = 'version_064';
 
 // Detect the base path dynamically
 var BASE_PATH = self.location.pathname.includes('/Pivot-App/') ? '/Pivot-App/' : '/';
